a832c21d8b891d7c16b32fd0bc3fe9b5f50986c5
Priyanshu Kumar tiwari <priyanshukumartiwari@Priyanshus-MacBook-Air.local>
2025-10-25 20:21:13 +0530

    Basic

---
+++ playwright.config.js
@@ -0,0 +1,42 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    headless: false,
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    
+ 
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
+
---
+++ tests/Basic.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+
+test("Selenium Web Page", async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    console.log(await page.url())
+
+    // const header1 = await page.locator('h1').textContent();
+    // console.log(header1)
+    // const header2 = await page.locator('h2').textContent();
+    // console.log(header2)
+
+    const row = page.locator('ul').locator('li').nth(5)
+    console.log(await row.textContent())
+    await row.click()
+
+    // await page.waitForLoadState('networkidle');
+    // console.log(await page.url())
+
+    await page.waitForLoadState('domcontentloaded'); 
+    console.log(await page.url())
+
+    const checkBox = page.locator('#checkboxes input').nth(0)
+    await checkBox.waitFor({ state: 'visible' });
+    await checkBox.click()
+    await expect(checkBox).toBeChecked();
+
+   // console.log(await locTable.textContent());
+    
+})
